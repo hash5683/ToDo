1380ac419376109b44fdc1221600a4be2809aecd
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-07-26 21:00:45 +0500

    Update urls.py

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginpage, name='login'),
-    path('logout/', views.logoutpage, name='logout'),
+    path('login/', views.loginview, name='login'),
+    path('logout/', views.logoutview, name='logout'),
     # Add other paths as needed
 ]
