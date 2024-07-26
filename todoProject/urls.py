4aecc98bae8813f819de01e4fa99a5c9d532980b
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-07-27 00:59:19 +0500

    Update urls.py

--- todoProject/urls.py
+++ todoProject/urls.py
@@ -27,5 +27,8 @@ urlpatterns = [
     path('' , include('registration.urls')),
     path('submit/', views.submit, name='submit'),
     path('create/', views.createpage, name='create'),
-    path('home/', views.homepage, name='home'),   
+    path('home/', views.homepage, name='home'),
+    path('delete/<int:todo_id>/', views.delete, name='delete'),
+    path('edit/<int:todo_id>/', views.edit, name='edit'),
+    path('mark_as_done/<int:todo_id>/', views.mark_as_done, name='mark_done'),
 ]
