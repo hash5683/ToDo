db22f27d24844f121625fc4c2991ced67c801ccb
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-07-26 11:56:40 +0500

    Delete authentification directory

--- authentification/urls.py
+++
@@ -1,9 +0,0 @@
-'''importing urls'''
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.loginpage, name='login'),
-    path('logout/', views.logoutpage, name='logout'),
-    # Add other paths as needed
-]
