22645e94c9c93071515e82157384b2941a044d68
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-07-27 00:58:41 +0500

    Update views.py

--- todo/views.py
+++ todo/views.py
@@ -17,8 +17,35 @@ def createpage(request):
         todo.save()
         return redirect('home')
     return render(request,'create.html')
+
+def delete(request, todo_id):
+    '''delete a todo'''
+    todo=Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('home')
+
+def edit(request, todo_id):
+    '''edit a todo'''
+    todo=Todo.objects.get(id=todo_id)
+    if request.method=='POST':
+        title=request.POST.get('title')
+        desc=request.POST.get('description')
+        todo.title=title
+        todo.description=desc
+        todo.save()
+        return redirect('home')
+    return render(request,'edit.html', {'todo': todo})
+
+def mark_as_done(request, todo_id):
+    '''mark a todo as done'''
+    todo=Todo.objects.get(id=todo_id)
+    todo.is_done=True
+    todo.save()
+    return redirect('home')
           
 def submit(request):
     '''Submit button on create page'''
     return render(request,'home.html')
+
+
  
