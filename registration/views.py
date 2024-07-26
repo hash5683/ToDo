cd3eaefe8b2f43352d29d0a47a7f317bf5211c66
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-07-26 20:54:17 +0500

    Update views.py

--- registration/views.py
+++ registration/views.py
@@ -1,19 +1,10 @@
-'''importing shortcuts and users'''
-from django.shortcuts import render, redirect, HttpResponse
-from django.contrib.auth.models import User
+'''importing the shortcuts and form'''
+from django.shortcuts import render
+from .forms import InputForm
+ 
 # Create your views here.
-def signuppage(request):
-    '''signup page'''
-    if request.method=='POST':
-        uname=request.POST.get('username')
-        email=request.POST.get('email')
-        pass1=request.POST.get('password1')
-        pass2=request.POST.get('password2')
-
-        if pass1!=pass2:
-            return HttpResponse("Your password and confrom password are not Same!!")
-        else:
-            my_user=User.objects.create_user(uname,email,pass1)
-            my_user.save()
-            return redirect('login')
-    return render (request,'signup.html')
+def signupview(request):
+    '''signup'''
+    context ={}
+    context['form']= InputForm()
+    return render(request, "signup.html", context)
