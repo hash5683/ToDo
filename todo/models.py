c9e2d96e4e141c86151706095609e0886b179f40
Hashim Ibrahim <169033727+hash5683@users.noreply.github.com>
2024-08-01 19:35:25 +0500

    Update models.py

--- todo/models.py
+++ todo/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Todo(models.Model):
-    '''A model'''
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
